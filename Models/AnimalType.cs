cf96574ebc9ee87f7f0b9e2037dbde9130adcf15
Camilobarre <barrecami@gmail.com>
2024-09-17 07:05:55 -0500

    feat: Se configuro la tabla en la clase AnimalType

--- Models/AnimalType.cs
+++ Models/AnimalType.cs
@@ -1,14 +1,19 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace API_Farm.Models
 {
+    [Table("animal_types")]
     public class AnimalType
     {
+        [Column("id")]
         public int Id { get; set; }
+        [Column("name")]
         public required string Name { get; set; } // Por ejemplo mamífero
+        [Column("description")]
         public string? Description { get; set; } // Por ejemplo son los tipos de animales que beben leche
 
     }
